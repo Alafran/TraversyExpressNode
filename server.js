16354b06af11155736d062697d6fa3b3a5ceacba
Alafran <33580422+Alafran@users.noreply.github.com>
2018-04-14 12:16:38 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,40 +1,81 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+//connect to DB
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check for db errors
+db.on('error', function(err) {
+  console.log(err);
+});
+
+// Check connection
+db.once('open',function() {
+  console.log('Connected to MongoDB');
+});
+
+//Bring in models
+let Article = require('./models/article');
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Set public folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//home route
+//renders the index template as homepage
 app.get('/', function(req,res) {
-  let articles = [
-    {
-      id:1,
-      title: 'Article One',
-      author: 'Joseph Woodside',
-      body: 'This is article one'
-    },
-    {
-      id:2,
-      title: 'Article Two',
-      author: 'Joey Rocket',
-      body: 'This is article two'
-    },
-    {
-      id:3,
-      title: 'Article Three',
-      author: 'JSide',
-      body: 'This is article three'
-    },
-  ];
-  res.render('index', {
-    title:'Articles',
-    articles: articles
+  Article.find({},function(err, articles) {
+    if(err) {
+      console.log(err);
+    } else {
+    res.render('index', {
+      title:'Articles',
+      articles: articles
+    });
+    }
   });
 });
 
+//Renders the add_article template when visit this URL
 app.get('/articles/add', function(req,res) {
   res.render('add_article', {
     title:'Add Article'
+  });
+});
+
+//Get single articles
+app.get('/article/:id',function(req,res) {
+  Article.findById(req.params.id, function(err, article) {
+    res.render('article', {
+      article:article
+    });
+  });
+});
+
+//Posts the article to the DB from the /articles/add route
+app.post('/articles/add', function(req,res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err) {
+      console.log(err);
+      return;
+    }
+    res.redirect('/');
   });
 });
 
